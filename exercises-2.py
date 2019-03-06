e1026d1f3363a0622b01a47aaebe6df5ed6d926e
Amit <amitko1351@gmail.com>
2019-03-06 16:58:57 +0200

    Fix the promte to use "\n"

--- exercises-2.py
+++ exercises-2.py
@@ -3,7 +3,7 @@ def sum_input_numbers():
     the function gets from the user the number to sum up
     """
     # The number the program gets
-    number = input("Please enter the number (enter stop for result)")
+    number = input("Please enter the number (enter stop for result)\n")
     # The sum of the numbers
     sum_of_numbers = 0
     while True:
@@ -12,7 +12,7 @@ def sum_input_numbers():
             break
         if number.isdigit():
             sum_of_numbers += int(number)
-        number = input("Please enter the number (enter stop for result)")
+        number = input("Please enter the number (enter stop for result)\n")
 
 
 def main():
