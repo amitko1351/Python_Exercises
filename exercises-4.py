49da09ec21776ce57a044c67ea313b27d2efcff0
Amit <amitko1351@gmail.com>
2019-03-06 20:17:55 +0200

    Add a function that compress the string

---
+++ exercises-4.py
@@ -0,0 +1,19 @@
+def compress_string(string):
+    # If there is no string return None
+    if not string:
+        return None
+    # Last char in the string
+    last_char = string[0]
+    # Count the number of repetition
+    count_repet = 0
+    # String that is going to be returned
+    new_string = ""
+    # Go through all the string
+    for char in string:
+        if char == last_char:
+            count_repet += 1
+        else:
+            new_string.join([last_char, str(count_repet)])
+            count_repet = 1
+            last_char = char
+    return new_string
