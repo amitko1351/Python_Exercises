7f13ebb41095fa1d453fbbbcfabff4124e1c4b68
Amit <amitko1351@gmail.com>
2019-03-06 21:46:07 +0200

    Add testing code

--- exercises-6.py
+++ exercises-6.py
@@ -6,3 +6,15 @@ def map_function(function_to_run, params):
         return_value = function_to_run(param)
         return_values.append(return_value)
     return return_values
+
+
+def mul(x,y):
+    return x*y
+
+
+def main():
+    return_list = map_function(mul, [(2, 3), (5, 2)])
+    print(str(return_list))
+
+if __name__ == '__main__':
+    main()
