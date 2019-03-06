af1a7c0fc4445f1dc740f89bbd108cf397293641
Amit <amitko1351@gmail.com>
2019-03-06 16:38:10 +0200

    Add the bank operation system

--- exercises-1.py
+++ exercises-1.py
@@ -45,9 +45,92 @@ class BankAccount:
         """
         return self.secret_code == secret_code
 
+
+def input_operation():
+    """
+    the function prints the menu and gets 
+    operation that the user want to do
+    :return: the number represent the operation 
+             that the user want to do
+    """
+    while True:
+        print("Enter the operation that you want to do:")
+        print("1 for printing the balance")
+        print("2 for taking money from the bank account")
+        print("3 for changing the secret code of the bank account")
+        print("4 for exit the system")
+        choice = int(input())
+        if 0 < choice < 5:
+            return choice
+
+
+def check_the_secret_code(bank_account):
+    """
+    the function gets the secret code from the user 
+    and check if the code is correct
+    :param bank_account: the bank account object
+
+    """
+    while True:
+        secret_code = input("Enter your secret code \n")
+        if bank_account.authenticate_account(secret_code):
+            break
+
+
+def take_money_from_account(bank_account):
+    """
+    the function gets from the user the amount
+    of money that he would like to take from the 
+    account (if he doesnt have enough money the function
+     prints a message)
+    :param bank_account: the user bank account object
+    """
+    print("How much money do you want to take?")
+    print("50 or 20 or other")
+    print("please enter the amount of the money")
+    amount = int(input())
+    if not bank_account.cash_withrawal(amount):
+        print("the cash withrawal failed you dont have enough money")
+
+
+def operate(operation, bank_account):
+    """
+    the function operate according to the user decisions
+    :param operation: the number represent the operation 
+                      that the user want to do
+    :param bank_account: the user bank account object
+    """
+    if operation == 1:
+        bank_account.print_balance()
+    elif operation == 2:
+        take_money_from_account(bank_account)
+    elif operation == 3:
+        secret_code = input("Enter you new code:")
+        bank_account.set_secret_code(secret_code)
+    else:
+        exit(0)
+
+
+def operate_bank_account(bank_account):
+    """
+    Operate on the bank account
+    :param bank_account: the user bank account object
+    """
+    # Operate on the bank account
+    while True:
+        # Check the secret code
+        check_the_secret_code(bank_account)
+        # Print the menu and gets the number of
+        # the operation
+        operation = input_operation()
+        # Operate According to the number entered
+        operate(operation, bank_account)
+
+
 def main():
     # Create a bank account object
-    bank_account = BankAccount(1234, 150)
+    bank_account = BankAccount("1234", 150)
+    operate_bank_account(bank_account)
 
 if __name__ == '__main__':
     main()
